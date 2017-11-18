04323163ac2714ca905e25ed5a29e62112b7bcbd
jplmoreira <jplmoreira92@gmail.com>
2017-11-18 16:57:06 +0000

    Add arm control with mouse

---
+++ DudeBank&Money/Assets/ArmRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour {
+
+    public int rotationOffset = 0;
+    private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; // Get position difference between the mouse cursor and the player
+        difference.Normalize();                                                                        // Normalize the vector
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;                          // Find the angle
+        if (m_FacingRight && ((rotZ > 90) || (rotZ < -90))) {
+            Flip();
+        } else if (!m_FacingRight && (rotZ < 90) && (rotZ > -90)) {
+            Flip();
+        }
+
+        if (m_FacingRight)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+        } else
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset - 180);
+        }
+	}
+
+    private void Flip() {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
--- DudeBank&Money/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ DudeBank&Money/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -20,6 +20,8 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
+        Transform playerSprite;             // Reference to the player sprite to change direction.
+
         private void Awake()
         {
             // Setting up references.
@@ -27,6 +29,11 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            playerSprite = transform.Find("Astronaut");
+            if (playerSprite == null)
+            {
+                Debug.LogError("No player sprites found");
+            }
         }
 
 
@@ -106,9 +113,9 @@ namespace UnityStandardAssets._2D
             m_FacingRight = !m_FacingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            Vector3 theScale = playerSprite.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+            playerSprite.localScale = theScale;
         }
     }
 }
