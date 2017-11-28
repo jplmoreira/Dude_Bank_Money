81294188d541c74e6b75fda688f52c5acf59a1c9
TomasMarques <tom_as_sm@hotmail.com>
2017-11-28 17:39:52 +0000

    jumpthrough platforms

---
+++ DudeBank&Money/Assets/Scripts/JumpPlatform.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class JumpPlatform : MonoBehaviour {
+	void OnCollisionStay2D(Collision2D collider){
+        if (collider.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.S)){ //&& Input.GetKeyDown("space") ->doesnt work, character jumps up :s
+            jumpThrough();
+            Invoke("jumpThrough", 0.5f);
+        }
+	}
+    public void jumpThrough(){
+        gameObject.GetComponent<Collider2D>().enabled = !gameObject.GetComponent<Collider2D>().enabled;
+    }
+}
