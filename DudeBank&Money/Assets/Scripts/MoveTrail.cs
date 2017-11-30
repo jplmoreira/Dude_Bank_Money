c6aea84ec62823e22100f2132b2c41cbc6aa67c5
jplmoreira <jplmoreira92@gmail.com>
2017-11-30 21:53:08 +0000

    Make bullets projectiles and behave accordingly

--- DudeBank&Money/Assets/Scripts/MoveTrail.cs
+++ DudeBank&Money/Assets/Scripts/MoveTrail.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class MoveTrail : MonoBehaviour {
 
-    public int bulletSpeed = 230;
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.right * Time.deltaTime * bulletSpeed);
-        Destroy(this.gameObject,1);
-	}
+    public int bulletSpeed = 100;
+    public Transform hitParticle;
+
+    private Rigidbody2D rigidBody;
+
+    private void Awake() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        GameObject barrel = GameObject.FindWithTag("GunBarrel");
+        if (barrel == null)
+            Debug.LogError("GunBarrel not found");
+        Vector2 bulletPosition = new Vector2(barrel.transform.position.x, barrel.transform.position.y);
+        rigidBody.AddForce((mousePosition - bulletPosition).normalized * bulletSpeed, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        rigidBody.velocity = new Vector2(0, 0);
+        ContactPoint2D contact = collision.contacts[0];
+        GameObject colliderObject = collision.collider.gameObject;
+        if (colliderObject.tag == "Enemy") {
+            EnemyScript enemy = colliderObject.GetComponent<EnemyScript>();
+            enemy.DamageEnemy(1);
+        }
+        Destroy(gameObject);
+        Transform particle = Instantiate(hitParticle, contact.point, Quaternion.FromToRotation(Vector3.right, contact.normal));
+        Destroy(particle.gameObject, 1f);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        rigidBody.velocity = new Vector2(0, 0);
+        ContactPoint2D contact = collision.contacts[0];
+        Destroy(gameObject);
+        Transform particle = Instantiate(hitParticle, contact.point, Quaternion.FromToRotation(Vector3.right, contact.normal));
+        Destroy(particle.gameObject, 1f);
+    }
 }
--- DudeBank&Money/Assets/Scripts/Weapon.cs
+++ DudeBank&Money/Assets/Scripts/Weapon.cs
@@ -2,110 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
-{
-    /// <summary>
-    /// Bullet prefab to be instantiated.
-    /// </summary>
-    public GameObject bullet;
-    /// <summary>
-    /// The speed at which the bullet moves.
-    /// </summary>
-    public float bulletSpeed = 20.0f;
+public class Weapon : MonoBehaviour {
 
     public int damage = 1;
     public int numBullets = 6;
-    public LayerMask hittable;
     public Transform bulletTrail;
     public Transform muzzleFlash;
-    public Transform hitParticle;
 
     public int Bullets {
-        get { return numBullets;  }
+        get { return numBullets; }
     }
 
     Transform barrel;
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake() {
         barrel = transform.Find("GunBarrel");
         if (barrel == null) {
             Debug.LogError("GunBarrel not found.");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            //Shoot();
-            Shoot2();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
         }
-	}
+    }
 
     void Shoot() {
         if (numBullets > 0) {
-            numBullets--;
-            Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
-                                            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            Vector2 barrelPosition = new Vector2(barrel.position.x, barrel.position.y);
-            RaycastHit2D hit = Physics2D.Raycast(barrelPosition, mousePosition - barrelPosition, 100, hittable);
-            Debug.DrawLine(barrelPosition, (mousePosition - barrelPosition) * 100);
-            if (hit.collider != null) {
-                Debug.DrawLine(barrelPosition, hit.point, Color.red);
-                EnemyScript enemy = hit.collider.GetComponent<EnemyScript>();
-                if (enemy != null)
-                    enemy.DamageEnemy(damage);
+            //numBullets--;
+            float rotation = transform.parent.rotation.eulerAngles.z;
+            if (transform.parent.localScale.x < 0) {
+                rotation += 180;
             }
-            Vector3 hitPos;
-            Vector3 hitNormal;
-            if (hit.collider == null) {
-                hitPos = (mousePosition - barrelPosition) * 30;
-                hitNormal = new Vector3(9999, 9999, 9999);
-            } else {
-                hitPos = hit.point;
-                hitNormal = hit.normal;
-            }
-            Effect(hitPos, hitNormal);
-        }
-    }
+            Instantiate(bulletTrail, barrel.position, Quaternion.Euler(0f, 0f, rotation));
 
-    void Shoot2()
-    {
-        if (numBullets > 0)
-        {
-            numBullets--;
-            Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
-                                            Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-            Vector2 barrelPosition = new Vector2(barrel.position.x, barrel.position.y);
-
-            GameObject aux = Instantiate(bullet, barrelPosition, Quaternion.identity);
-            aux.GetComponent<Rigidbody2D>().velocity = (mousePosition - barrelPosition) * bulletSpeed * Time.deltaTime;
+            Transform clone = (Transform)Instantiate(muzzleFlash, barrel.position, barrel.rotation);
+            clone.parent = barrel;
+            float size = Random.Range(0.6f, 0.9f);
+            clone.localScale = new Vector3(size, size, 0);
+            Destroy(clone.gameObject, 0.02f);
         }
-    }
-
-    void Effect(Vector3 hitPos, Vector3 hitNormal) {
-        float rotation = transform.parent.rotation.eulerAngles.z;
-        if (transform.parent.localScale.x < 0) {
-            rotation += 180;
-        }
-        Transform trail = (Transform) Instantiate(bulletTrail, barrel.position, Quaternion.Euler(0f, 0f, rotation));
-        LineRenderer lr = trail.GetComponent<LineRenderer>();
-        if (lr != null) {
-            lr.SetPosition(0, barrel.position);
-            lr.SetPosition(1, hitPos);
-        }
-        Destroy(trail.gameObject, 0.05f);
-
-        if (hitNormal != new Vector3(9999,9999,9999)) {
-            Transform particle = Instantiate(hitParticle, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal));
-            Destroy(particle.gameObject, 1f);
-        }
-
-        Transform clone = (Transform) Instantiate(muzzleFlash, barrel.position, barrel.rotation);
-        clone.parent = barrel;
-        float size = Random.Range(0.6f, 0.9f);
-        clone.localScale = new Vector3(size, size, 0);
-        Destroy(clone.gameObject, 0.02f);
     }
 }
