6e2260ead72095cdd567996150ababefaf3760ca
Ricardo Fonseca <ric.bicas@gmail.com>
2017-11-30 19:26:45 +0000

    Adds Bullet script and its collider. Tries to use the collider as trigger to play animation. Still not working

---
+++ DudeBank&Money/Assets/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform bulletTrail;
+    public Transform muzzleFlash;
+    public Transform hitParticle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Effect();
+    }
+
+    void Effect()
+    {
+        Transform particle = Instantiate(hitParticle, gameObject.transform.position, Quaternion.identity);
+        Destroy(particle.gameObject, 1f);
+    }
+}
