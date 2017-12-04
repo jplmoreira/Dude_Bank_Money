93f23af4e99fb02cca037acf97e0c884b818de8f
D3ath22 <pedro.monteiro22@gmail.com>
2017-12-04 15:50:40 +0000

    imported missing scenes
    -its now working
    -error on unload because not checking what is load, but its ok.

--- DudeBank&Money/Assets/Scripts/GameMaster.cs
+++ DudeBank&Money/Assets/Scripts/GameMaster.cs
@@ -3,36 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour
+{
 
     public static GameMaster gm;
 
-    public IEnumerator RestartGame() {
-        yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public static void EndLevel() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-
-    public static void KillCharacter(CharacterScript character) {
-        Destroy(character.gameObject);
+    public IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene("Menu 3D");
+        SceneManager.UnloadSceneAsync("GameOver");
+        SceneManager.UnloadSceneAsync("GameWon");
         SceneManager.UnloadSceneAsync("Prototype");
     }
 
-    private void Start() {
-        if (gm == null) {
+    public static void EndLevel()
+    {
+        SceneManager.LoadScene("GameWon", LoadSceneMode.Additive);
+        gm.StartCoroutine(gm.RestartGame());
+    }
+
+    public static void KillCharacter(CharacterScript character)
+    {
+        Destroy(character.gameObject);
+        SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
+        gm.StartCoroutine(gm.RestartGame());
+    }
+
+    public static void KillEnemy(EnemyScript character)
+    {
+        Destroy(character.gameObject);
+        
+    }
+
+    private void Start()
+    {
+        if (gm == null)
+        {
             gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
         }
     }
 
-    private void Update() {
+    private void Update()
+    {
         GameObject player = GameObject.FindWithTag("Player");
         if (player == null)
             gm.StartCoroutine(gm.RestartGame());
-        if (Input.GetKeyDown(KeyCode.Escape)) {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             SceneManager.LoadScene("Menu 3D", LoadSceneMode.Additive);
         }
     }
