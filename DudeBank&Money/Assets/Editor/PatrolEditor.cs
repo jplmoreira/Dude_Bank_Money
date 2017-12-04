66d8f174e00252c8078055f3d34aa5e06e01b0fe
Ricardo Fonseca <ric.bicas@gmail.com>
2017-12-04 01:21:45 +0000

    Adds patrol behaviour, and test scene for it

---
+++ DudeBank&Money/Assets/Editor/PatrolEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameMaster))]
+public class PatrolEditor : Editor
+{
+    private static bool mEditMode = true;
+    private static bool mEditMode2 = false;
+
+    private void OnSceneGUI()
+    {
+        if (mEditMode)
+        {
+            if (Event.current.type == EventType.layout)
+            {
+                HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
+            }
+
+            if (Event.current.type == EventType.MouseDown)
+            {
+                Vector2 mousePos = Event.current.mousePosition;
+                mousePos.y = Camera.current.pixelHeight - mousePos.y;
+                Vector3 position = Camera.current.ScreenPointToRay(mousePos).origin;
+                /*GameObject go = new GameObject("StartPoint" + 1);
+                go.transform.position = position;*/
+                GameMaster aux = FindObjectOfType<GameMaster>();
+                if (aux == null)
+                {
+                    Debug.Log("Not found");
+                }
+                else
+                {
+                    Debug.Log("Found");
+                }
+            }
+        }
+    }
+}
--- DudeBank&Money/Assets/Scripts/EnemyScript.cs
+++ DudeBank&Money/Assets/Scripts/EnemyScript.cs
@@ -2,22 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
+    public Vector3 pos1;
+    public Vector3 currentEndPos;
+    public Vector3 pos2;
 
     [System.Serializable]
-    public class EnemyStats {
+    public class EnemyStats
+    {
         public int health = 1;
     }
 
     public EnemyStats enemyStats = new EnemyStats();
 
-    public void DamageEnemy(int amount) {
-        enemyStats.health -= amount;
-        if (enemyStats.health <= 0) 
-            GameMaster.KillEnemy(this);
+    private void Start()
+    {
+        pos1 = transform.position;
+        currentEndPos = pos2;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void Update()
+    {
+        if (currentEndPos == pos2 && Vector3.Distance(transform.position, pos2) < 0.1f)
+        {
+            currentEndPos = pos1;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (currentEndPos == pos1 && Vector3.Distance(transform.position, pos1) < 0.1f)
+        {
+            currentEndPos = pos2;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        transform.position = Vector3.Lerp(transform.position, currentEndPos, Time.deltaTime * 1.0f);
+    }
+
+    public void DamageEnemy(int amount)
+    {
+        enemyStats.health -= amount;
+        if (enemyStats.health <= 0)
+        {
+            GameMaster.KillEnemy(this);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         PlayerScript player = collision.collider.GetComponent<PlayerScript>();
         if (player != null) {
             player.DamagePlayer();
