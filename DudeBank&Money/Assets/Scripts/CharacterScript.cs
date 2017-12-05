39bff98a6c411a14113d082aab0e5ba07459111d
D3ath22 <pedro.monteiro22@gmail.com>
2017-12-05 01:34:56 +0000

    Added hazard

--- DudeBank&Money/Assets/Scripts/CharacterScript.cs
+++ DudeBank&Money/Assets/Scripts/CharacterScript.cs
@@ -11,6 +11,12 @@ public class CharacterScript : MonoBehaviour {
 
     public CharacterStats charStats = new CharacterStats();
 
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (gameObject.tag == "Enemy" && coll.gameObject.name == "MaceOnChain01")
+            DamageCharacter(1);
+    }
+
     public void DamageCharacter(int damage) {
         charStats.health -= damage;
         if (charStats.health <= 0)
---
+++ DudeBank&Money/Assets/Scripts/TrapScr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScr : MonoBehaviour
+{
+
+    private Animator anim;
+    
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        anim.enabled = true;
+    }
+}
