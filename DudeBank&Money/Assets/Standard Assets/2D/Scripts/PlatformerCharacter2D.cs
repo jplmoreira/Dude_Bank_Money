07dced80578c2d48f4e60ad8942d55ccec62ed41
jplmoreira <jplmoreira92@gmail.com>
2017-12-30 19:14:38 +0000

    Update sprites

--- DudeBank&Money/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ DudeBank&Money/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -34,7 +34,7 @@ namespace UnityStandardAssets._2D
             currSpeed = m_MaxSpeed;
             m_GroundCheck = transform.Find("GroundCheck");
             m_CeilingCheck = transform.Find("CeilingCheck");
-            m_Anim = GetComponent<Animator>();
+            //m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
             playerSprite = transform.Find("Astronaut");
             if (playerSprite == null)
@@ -56,10 +56,10 @@ namespace UnityStandardAssets._2D
                 if (colliders[i].gameObject != gameObject)
                     m_Grounded = true;
             }
-            m_Anim.SetBool("Ground", m_Grounded);
+            //m_Anim.SetBool("Ground", m_Grounded);
 
             // Set the vertical animation
-            m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
+            //m_Anim.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
             if (m_Rigidbody2D.velocity.y < 0 && timeStop) {
                 m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
                 m_Rigidbody2D.gravityScale = 0;
@@ -74,7 +74,7 @@ namespace UnityStandardAssets._2D
         public void Move(float move, bool crouch, bool jump)
         {
             // If crouching, check to see if the character can stand up
-            if (!crouch && m_Anim.GetBool("Crouch"))
+            if (!crouch /*&& m_Anim.GetBool("Crouch")*/)
             {
                 // If the character has a ceiling preventing them from standing up, keep them crouching
                 if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
@@ -84,7 +84,7 @@ namespace UnityStandardAssets._2D
             }
 
             // Set whether or not the character is crouching in the animator
-            m_Anim.SetBool("Crouch", crouch);
+            //m_Anim.SetBool("Crouch", crouch);
 
             //only control the player if grounded or airControl is turned on
             if (m_Grounded || m_AirControl)
@@ -108,7 +108,7 @@ namespace UnityStandardAssets._2D
                 move = (crouch ? move*m_CrouchSpeed : move);
 
                 // The Speed animator parameter is set to the absolute value of the horizontal input.
-                m_Anim.SetFloat("Speed", Mathf.Abs(move));
+                //m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
                 m_Rigidbody2D.velocity = new Vector2(move*currSpeed, m_Rigidbody2D.velocity.y);
@@ -116,12 +116,12 @@ namespace UnityStandardAssets._2D
                 
             }
             // If the player should jump...
-            if (m_Grounded && jump && m_Anim.GetBool("Ground"))
+            if (m_Grounded && jump /*&& m_Anim.GetBool("Ground")*/)
             {
                 if (timeStop && timeStopActions > 0)
                 {
                     m_Grounded = false;
-                    m_Anim.SetBool("Ground", false);
+                    //m_Anim.SetBool("Ground", false);
                     m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
                     timeStopActions--;
                 }
@@ -129,7 +129,7 @@ namespace UnityStandardAssets._2D
                 {
                     // Add a vertical force to the player.
                     m_Grounded = false;
-                    m_Anim.SetBool("Ground", false);
+                    //m_Anim.SetBool("Ground", false);
                     m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
                 }
             }
