ce344a84c935b91570c912545f2b5e1832f25544
Ricardo Fonseca <ric.bicas@gmail.com>
2018-01-09 00:35:52 +0000

    Adds projectile trajectory. Needs tweaking.

--- DudeBank&Money/Assets/Scripts/TNTWeapon.cs
+++ DudeBank&Money/Assets/Scripts/TNTWeapon.cs
@@ -18,6 +18,10 @@ public class TNTWeapon : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                                   Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        Vector3 dir = mousePosition - barrel.position;
+        UpdateTrajectory(barrel.position, dir.normalized * throwSpeed * Time.deltaTime, new Vector3(0.0f, -9.8f, 0.0f));
         if (Input.GetButtonDown("Fire1"))
         {
             float rotation = transform.parent.rotation.eulerAngles.z;
@@ -26,9 +30,9 @@ public class TNTWeapon : MonoBehaviour
                 rotation += 180;
             }
             GameObject clone = (GameObject)Instantiate(tntThrowable, barrel.position, Quaternion.Euler(0f, 0f, rotation));
-            Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
-                                                Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
-            Vector3 dir = mousePosition - barrel.position;
+            //Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+            //                                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            //Vector3 dir = mousePosition - barrel.position;
             tntRigid = clone.GetComponent<Rigidbody2D>();
             tntRigid.AddForce(Time.deltaTime * throwSpeed * dir.normalized, ForceMode2D.Impulse);
         }
@@ -45,6 +49,40 @@ public class TNTWeapon : MonoBehaviour
         {
             Debug.Log("STOPPED");
             Gizmos.DrawWireSphere(tntRigid.transform.position, 5.0f);
+        }
+    }
+
+
+    void UpdateTrajectory(Vector3 initialPosition, Vector3 initialVelocity, Vector3 gravity)
+    {
+        int numSteps = 50; // for example
+        float timeDelta = 1.0f / initialVelocity.magnitude; // for example
+
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = numSteps;
+
+        Vector3 position = initialPosition;
+        Vector3 lastPos = position;
+        Vector3 velocity = initialVelocity;
+
+        int i = 0;
+        Vector2 contactPoint;
+        while ((contactPoint = Physics2D.Linecast(new Vector2(lastPos.x, lastPos.y), new Vector2(position.x, position.y)).point) == Vector2.zero && i < numSteps)
+        {
+            lineRenderer.SetPosition(i, position);
+            lastPos = position;
+            position += velocity * timeDelta + 0.5f * gravity * timeDelta * timeDelta;
+            velocity += gravity * timeDelta;
+            i++;
+        }
+        if (i != numSteps)
+        {
+            lineRenderer.SetPosition(i, contactPoint);
+            i++;
+            for (int j = i; j < numSteps; j++)
+            {
+                lineRenderer.SetPosition(j, lastPos);
+            }
         }
     }
 
