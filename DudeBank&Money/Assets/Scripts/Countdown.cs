5adce0d761b11827805e5bea5f62957a302614e1
D3ath22 <pedro.monteiro22@gmail.com>
2018-01-12 16:02:45 +0000

    Fix alarm in level 1

--- DudeBank&Money/Assets/Scripts/Countdown.cs
+++ DudeBank&Money/Assets/Scripts/Countdown.cs
@@ -14,9 +14,14 @@ public class Countdown : MonoBehaviour {
 
     private PlayerScript player;
 
+    AudioSource[] audios;
+    AudioSource alarmSound;
+    private bool audioPlayed;
+
     private void Awake() {
         GameObject obj = GameObject.Find("Player");
         player = obj.GetComponent<PlayerScript>();
+        audioPlayed = false;
     }
 
     // Update is called once per frame
@@ -32,10 +37,14 @@ public class Countdown : MonoBehaviour {
 
     public void SoundAlarm() {
         alarm = true;
-        AudioSource[] audios = GetComponents<AudioSource>();
-        AudioSource alarmSound = audios[1];
-        if (!alarmSound.isPlaying)
+        audios = GetComponents<AudioSource>();
+        alarmSound = audios[1];
+        if (!alarmSound.isPlaying && !audioPlayed)
+        {
+            alarmSound.time = 11;
             alarmSound.Play();
+            audioPlayed = true;
+        }
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         for (int i = 0; i < enemies.Length; i++)
             enemies[i].GetComponent<EnemyScript>().Alarmed();
